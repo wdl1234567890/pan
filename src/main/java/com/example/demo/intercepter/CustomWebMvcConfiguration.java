600fdede6f263997ebd1d8661a8541335cf6790e
fuling <1639913166@qq.com>
2020-09-22 22:57:41 +0800

    小修改

--- src/main/java/com/example/demo/intercepter/CustomWebMvcConfiguration.java
+++ src/main/java/com/example/demo/intercepter/CustomWebMvcConfiguration.java
@@ -21,7 +21,7 @@ public class CustomWebMvcConfiguration implements WebMvcConfigurer{
 	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(loginStateInterceptor()).addPathPatterns("/api/v1/prii/**");
+		registry.addInterceptor(loginStateInterceptor()).addPathPatterns("/api/v1/pri/**");
 		WebMvcConfigurer.super.addInterceptors(registry);
 	}
 
